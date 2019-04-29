ab2e9e3a64d9948596cc28499d219c5b52e3a21a
msacli <mustafasacli70@gmail.com>
2019-04-29 10:29:34 +0300

    new nuget vesion.

--- MstDexterSolution/Mst.Dexter.Extensions/Properties/AssemblyInfo.cs
+++ MstDexterSolution/Mst.Dexter.Extensions/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Mst.Mst.Dexter.Extensions")]
+[assembly: AssemblyTitle("Mst.Dexter.Extensions")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Mst.Mst.Dexter.Extensions")]
+[assembly: AssemblyCompany("msacli")]
+[assembly: AssemblyProduct("Mst.Dexter.Extensions")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.1.0.1")]
+[assembly: AssemblyFileVersion("1.1.0.1")]
