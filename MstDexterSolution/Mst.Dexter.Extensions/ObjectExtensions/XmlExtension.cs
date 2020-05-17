e88cf5e2c0fafead32d6d8bf9f4530de479c5a13
mustafasacli <mustafasacli70@gmail.com>
2020-05-17 16:58:58 +0300

    xml etxensions added.

---
+++ MstDexterSolution/Mst.Dexter.Extensions/ObjectExtensions/XmlExtension.cs
@@ -0,0 +1,39 @@
+﻿namespace Mst.Dexter.Extensions.ObjectExtensions
+{
+    using System.Collections.Generic;
+    using System.Dynamic;
+    using System.Xml;
+
+    public static class XmlExtension
+    {
+        /// <summary>
+        /// Get xml nodes as expando object list.
+        /// </summary>
+        /// <param name="nodeList"></param>
+        /// <returns>expando object list</returns>
+        public static List<ExpandoObject> GetNodesAsDynamic(this XmlNodeList nodeList)
+        {
+            var list = new List<ExpandoObject>();
+
+            if (nodeList == null || nodeList.Count < 1)
+                return list;
+            foreach (XmlNode node in nodeList)
+            {
+                var childNodes = node.ChildNodes;
+                if (childNodes == null)
+                    continue;
+
+                IDictionary<string, object> dictionary = new ExpandoObject();
+                foreach (XmlNode childNode in childNodes)
+                {
+                    dictionary[childNode.Name] = childNode.InnerXml;
+                }
+
+                ExpandoObject expando = dictionary as ExpandoObject;
+                list.Add(expando);
+            }
+
+            return list;
+        }
+    }
+}
